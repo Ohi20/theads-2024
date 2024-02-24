e72d5414ec641062f592c182ef2d3a5351242e92
Ohi20 <ashrafohi20@gmail.com>
2024-02-24 16:18:04 +0600

    communities-added

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,14 +1,14 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import { fetchUser } from "@/lib/actions/user.actions";
 import AccountProfile from "@/components/forms/AccountProfile";
-import { fetchUsers } from "@/lib/actions/user.actions";
 
 async function Page() {
   const user = await currentUser();
   if (!user) return null; // to avoid typescript warnings
 
-  const userInfo = await fetchUsers(user.id);
+  const userInfo = await fetchUser(user.id);
   if (userInfo?.onboarded) redirect("/");
 
   const userData = {
