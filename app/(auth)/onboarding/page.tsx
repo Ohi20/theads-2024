be70c9d542d94b4e075a66602685b3ce44316c67
Ohi20 <ashrafohi20@gmail.com>
2024-02-24 12:37:40 +0600

    postthread-created

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,19 +1,25 @@
-import AccountProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUsers } from "@/lib/actions/user.actions";
 
 async function Page() {
   const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
 
-  const userInfo = {};
+  const userInfo = await fetchUsers(user.id);
+  if (userInfo?.onboarded) redirect("/");
 
   const userData = {
-    id: user?.id,
+    id: user.id,
     objectId: userInfo?._id,
     username: userInfo ? userInfo?.username : user.username,
     name: userInfo ? userInfo?.name : user.firstName ?? "",
     bio: userInfo ? userInfo?.bio : "",
     image: userInfo ? userInfo?.image : user.imageUrl,
   };
+
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
       <h1 className="head-text">Onboarding</h1>
